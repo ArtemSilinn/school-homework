0a1432ab4f74530418db6cf0d2de7018111c6c74
ArtemSilinn <44139631+ArtemSilinn@users.noreply.github.com>
2018-11-10 21:17:24 +0300

    Create 2.cpp

---
+++ 11.11.18-hw3-prime/2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+bool ch(int a)
+{
+ return ((a % 2) == 0);
+}
+
+int main()
+{
+ int n, b, c, d = 1;
+ cin » n;
+
+ b = (n % 10);
+ c = b;
+
+ while (n >= 1)
+ {
+  if (ch(c) == ch(b))
+  {
+   d*= c;
+  };
+  n /= 10;
+  c = (n % 10);
+ };
+ cout « d;
+}
