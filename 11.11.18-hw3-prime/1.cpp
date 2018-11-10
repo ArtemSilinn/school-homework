1aea3b4a4c7f1caf131141089e26d543d93de8b6
ArtemSilinn <44139631+ArtemSilinn@users.noreply.github.com>
2018-11-10 21:16:52 +0300

    Create 1.cpp

---
+++ 11.11.18-hw3-prime/1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m, s = 0;
+
+    cin » n;
+    m = (n % 10);
+
+    while (n >= 1)
+    {
+        if ((m% 2) == 1)
+        {
+            s += m;
+        };
+        n /= 10;
+        m= (n % 10);
+    };
+
+    cout « s;
+}
