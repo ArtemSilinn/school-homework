a69ca238dde516a4d84273102e1b6990402f0bc4
ArtemSilinn <44139631+ArtemSilinn@users.noreply.github.com>
2018-12-26 22:49:23 +0300

    Create 3.cpp

---
+++ Hw5-26.12/3.cpp
@@ -0,0 +1,35 @@
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+bool search(int *A, int size,int x,int q, int p = 0) {
+	if (A[(q + p) / 2] == x)
+		return true;
+
+	if (q - p <= 1)
+		return false;
+
+	if (A[(p + q) / 2] > x) 
+		search(A, size, x, (p + q) / 2, p);
+	
+
+	if (A[(p + q) / 2] < x) 
+		search(A, size, x, q, (p + q) / 2);
+}
+int main()
+{
+	int size;
+	cin >> size;
+	int *A = new int[size];
+	for (int i = 0; i < size; i++) {
+		cin >> A[i];
+	}
+	int a, b;
+	cin >> a >> b;
+	if (search(A, size, a, size) && search(A, size, b, size) == true)
+		cout << " YES ";
+	else cout << " NO";
+	
+	delete[] A;
+	return 0;
+}
